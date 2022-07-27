6bcbaa355ecc4a4da1302e65af13aedda6628b83
ttasjwi <ttasjwi920@gmail.com>
2022-07-27 16:12:08 +0900

    Initial commit

---
+++ src/main/java/com/ttasjwi/springbasicsecurity/Main.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.springbasicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
