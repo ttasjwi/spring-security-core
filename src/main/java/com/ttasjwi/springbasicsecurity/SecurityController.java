2f82e0199fe5dcbdd4265904bc7fa853539d5cf6
ttasjwi <ttasjwi920@gmail.com>
2023-07-09 17:49:50 +0900

    [ch1-1] 홈 컨트롤러 셋팅

---
+++ src/main/java/com/ttasjwi/springbasicsecurity/SecurityController.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.springbasicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
