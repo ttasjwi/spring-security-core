c03263e418de446bdaf6199feb6aab50d3dff48f
ttasjwi <ttasjwi920@gmail.com>
2024-03-08 16:16:16 +0900

    익명 사용자 인증

--- src/main/kotlin/com/security/core/config/SecurityConfig.kt
+++ src/main/kotlin/com/security/core/config/SecurityConfig.kt
@@ -1,19 +1,13 @@
 package com.security.core.config
 
-import com.security.core.util.getLogger
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig(
-    private val userDetailsService: UserDetailsService
-) {
-
-    private val logger = getLogger(javaClass)
+class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -22,15 +16,13 @@ class SecurityConfig(
                 authorize(anyRequest, authenticated)
             }
             formLogin { }
-            rememberMe {
-                rememberMeParameter = "remember" // 파라미터명 : 기본 파라미터명은 remember-me
-                tokenValiditySeconds = 3600 // 토큰이 유효한 시간 : 초 단위 설정, 기본 값은 14일(2주)
-                // alwaysRemember = true // 사용자가 리멤버 미를 활성화시키는 작업 없이도 항상 실행 (일반적으로 false)
-                userDetailsService = userDetailsService() // 리멤버미 인증 작업에서 사용자 계정을 조회하는 작업 수행. 반드시 필요...
+
+            // 따로 설정 안해도 생성되긴 함
+            anonymous {
+
             }
+
         }
         return http.build()
     }
-
-    private fun userDetailsService() = userDetailsService
 }
