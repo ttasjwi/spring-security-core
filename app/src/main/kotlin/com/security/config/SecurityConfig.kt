402d1f05083ba42287873a79aaedddb493c75262
ttasjwi <ttasjwi920@gmail.com>
2024-05-26 17:23:15 +0900

    요청 기반 인가 관리자

--- app/src/main/kotlin/com/security/config/SecurityConfig.kt
+++ app/src/main/kotlin/com/security/config/SecurityConfig.kt
@@ -2,59 +2,39 @@ package com.security.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.config.core.GrantedAuthorityDefaults
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager
 
 
 @EnableWebSecurity
 @Configuration
 class SecurityConfig {
 
-//    @Bean
-//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-//        http {
-//            authorizeHttpRequests {
-//                authorize("/user", hasRole("USER"))
-//                authorize("/admin", hasRole("ADMIN"))
-//                authorize("/db", hasRole("DB"))
-//                authorize(anyRequest, authenticated)
-//            }
-//            formLogin { }
-//        }
-//        return http.build()
-//    }
-
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .authorizeHttpRequests {
-                it
-                    .requestMatchers("/user").hasRole("USER")
-                    .requestMatchers("/admin").hasRole("ADMIN")
-                    .requestMatchers("/db").hasRole("DB")
-                    .anyRequest().authenticated()
+        http {
+            authorizeHttpRequests {
+                authorize("/user", hasRole("USER"))
+                authorize("/admin", hasAuthority("ROLE_ADMIN"))
+                authorize("/db", WebExpressionAuthorizationManager("hasRole('DB')"))
+                authorize(anyRequest, authenticated)
             }
-            .formLogin(Customizer.withDefaults())
+            formLogin { }
+        }
         return http.build()
     }
 
     @Bean
-    fun grantedAuthorityDefaults(): GrantedAuthorityDefaults {
-        return GrantedAuthorityDefaults("MYPREFIX_")
-    }
-
-    @Bean
     fun userDetailsService(): UserDetailsService {
-        val user = User.withUsername("user").password("{noop}1111").authorities("MYPREFIX_USER").build()
-        val db = User.withUsername("db").password("{noop}1111").authorities("MYPREFIX_DB").build()
-        val admin = User.withUsername("admin").password("{noop}1111").authorities("MYPREFIX_ADMIN").build()
+        val user = User.withUsername("user").password("{noop}1111").roles("USER").build()
+        val db = User.withUsername("db").password("{noop}1111").roles("DB").build()
+        val admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN").build()
         return InMemoryUserDetailsManager(user, db, admin)
     }
 }
